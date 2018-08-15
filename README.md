865f776b5b1d97a4cd4fc86f7496f48e9c237027
Mohammad <mohammad.h.ahmad@gmail.com>
2018-08-15 14:26:19 +0300

    init commit

---
+++ README.md
@@ -0,0 +1,31 @@
+Yii2 dompdf
+===========
+This is a yii2 extension for the dompdf plugin
+
+Installation
+------------
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+```
+php composer.phar require --prefer-dist mohammadahmad/yii2-dompdf "*"
+```
+
+or add
+
+```
+"mohammadahmad/yii2-dompdf": "*"
+```
+
+to the require section of your `composer.json` file.
+
+
+Usage
+-----
+
+Once the extension is installed, simply use it in your code by  :
+
+```php
+<?= \mohammadahmad\yii2Dompdf\AutoloadExample::widget(); ?>```
---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once 'lib/html5lib/Parser.php';
+require_once 'lib/php-font-lib/src/FontLib/Autoloader.php';
+require_once 'lib/php-svg-lib/src/autoload.php';
+require_once 'src/Autoloader.php';
+Dompdf\Autoloader::register();
+
+// reference the Dompdf namespace
+use Dompdf\Dompdf;
+
+// instantiate and use the dompdf class
+$dompdf = new Dompdf();
+$dompdf->loadHtml('hello world');
+
+// (Optional) Setup the paper size and orientation
+$dompdf->setPaper('A4', 'landscape');
+
+// Render the HTML as PDF
+$dompdf->render();
+
+// Output the generated PDF to Browser
+$dompdf->stream();
